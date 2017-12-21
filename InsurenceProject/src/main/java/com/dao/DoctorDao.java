d3e765c0ce5b3d0b908b2f4232ae96d63a91a506
Sviatoslav Naida <sviatoslav.naida@gmail.com>
2017-12-21 15:11:44 -0500

    del

--- InsurenceProject/src/main/java/com/dao/DoctorDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.entity.Doctor;
-
-public interface DoctorDao extends CrudDao<Doctor, Integer>{
-
-}
--- InsurenceProject/src/main/java/com/service/DoctorTypeService.java
+++
@@ -1,7 +0,0 @@
-package com.service;
-
-import com.entity.DoctorType;
-
-public interface DoctorTypeService extends CrudService<DoctorType, Integer>{
-
-}
